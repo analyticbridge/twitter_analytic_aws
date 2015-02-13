43e2e45503aac092f63e821235fece5715b8e577
Dave asp <analyticbridge@gmail.com>
2015-02-13 16:17:59 -0500

    sentiment_aws_script
    using tweepy ,aws cloudfront template scripts

---
+++ twitterparam.py
@@ -0,0 +1,4 @@
+OAuthConsKey = 'ndwH4oAFYjmgH7mK8C2auKfA2'
+OAuthConsSecret = 'kqrmiL8s4NL9eoWzDHkUcRzvkYNInRjffm7hrSHgp28ixnW3zD'
+OAuthToken = '3033374511-LKirjKd8MshtKuYIUHEzZzkjyKsCUzgpjr0fyri'
+OAuthTokenSecret = 'lHFm0OUNoRmTHTaYPfI1dNNqdyeM09DEBM9jqsHti8AUk'[ec2-user@ip-172-31-45-244 sentiment]$
